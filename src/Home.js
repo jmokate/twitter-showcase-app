11e6015104d0dddcac62b12251336e85d0ad6af9
jmokate <jmokate@gmail.com>
2020-02-09 13:54:30 -0600

    removed some files

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Row, Col } from "react-bootstrap";
 
 function Home() {
   const homeIMG =
     "https://images.unsplash.com/photo-1517816428104-797678c7cf0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
 
   return (
-    <div>
-      <Container className='btn-container'>
-        <Button variant='outline-primary' className='home-btn'>
-          Home
-        </Button>
-        <Button variant='outline-primary' className='search-btn'>
-          Search
-        </Button>
-        <Button variant='outline-primary' className='random-btn'>
-          Random Tweets
-        </Button>
-      </Container>
-      <img className='homeIMG' src={homeIMG} alt='megaphone' />
-    </div>
+    <Container className='btn-container'>
+      <Row>
+        <Col>
+          <Button variant='outline-primary' size='lg' className='home-btn'>
+            Home
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='outline-primary' size='lg' className='search-btn'>
+            Search
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='outline-primary' size='lg' className='random-btn'>
+            Random Tweets
+          </Button>
+        </Col>
+      </Row>
+
+      <img
+        class='img-responsive'
+        className='homeIMG'
+        src={homeIMG}
+        alt='megaphone'
+      />
+    </Container>
   );
 }
 
