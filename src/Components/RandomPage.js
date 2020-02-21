5f8a799d56f8e895a2020318372eaf7fc537b354
jmokate <jmokate@gmail.com>
2020-02-20 18:20:18 -0600

    updated code

--- src/Components/RandomPage.js
+++ src/Components/RandomPage.js
@@ -1,24 +1,35 @@
 import React from "react";
-import { Image, Card, CardGroup, Container, Col, Row } from "react-bootstrap";
-import { FaComment, FaHeart, FaRetweet } from "react-icons/fa";
+import { Card, CardGroup, Col, Row } from "react-bootstrap";
+
 import RandomTweet from "./RandomTweet";
 
 class RandomPage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       userClicked: "",
-      returnedTweet: [],
-      userImg: "",
-      userName: "",
-      userHandle: "",
-      datePosted: "",
-      tweetBody: "",
-      comments: 0,
-      retweets: 0,
-      likes: 0
+      returnedTweet: [
+        {
+          userImg:
+            "https://pbs.twimg.com/profile_images/875392068125769732/yrN-1k0Y_400x400.jpg",
+          userName: "The Onion",
+          userHandle: "theonion",
+          datePosted: "4h",
+          tweetBody: "this is just a test",
+          comments: 5,
+          retweets: 9,
+          likes: 55
+        }
+      ]
     };
+    this.generateRandomTweet = this.generateRandomTweet.bind(this);
   }
+
+  generateRandomTweet() {
+    console.log("cliked");
+    // const randomTweet =
+  }
+
   render() {
     return (
       <div>
@@ -28,37 +39,61 @@ class RandomPage extends React.Component {
             <h2>Click a user below and view some random Tweets!</h2>
           </Col>
         </Row>
-
         <CardGroup>
-          <Card bg='dark' text='light' border='light'>
+          <Card
+            bg='dark'
+            text='light'
+            border='light'
+            onClick={() => this.generateRandomTweet()}
+          >
             <Card.Img
               variant='top'
               src='https://pbs.twimg.com/profile_images/875392068125769732/yrN-1k0Y_400x400.jpg'
             />
             <Card.Title align='center'>The Onion</Card.Title>
           </Card>
-          <Card bg='dark' text='light' border='light'>
+          <Card
+            bg='dark'
+            text='light'
+            border='light'
+            onClick={() => this.generateRandomTweet()}
+          >
             <Card.Img
               variant='top'
               src='https://pbs.twimg.com/profile_images/1208165423109292032/_oEEIsvx_400x400.jpg'
             />
             <Card.Title align='center'>NPR</Card.Title>
           </Card>
-          <Card bg='dark' text='light' border='light'>
+          <Card
+            bg='dark'
+            text='light'
+            border='light'
+            onClick={() => this.generateRandomTweet()}
+          >
             <Card.Img
               variant='top'
               src='https://pbs.twimg.com/profile_images/1216015585399705601/Hs-9iDoo_400x400.jpg'
             />
             <Card.Title align='center'>Chicago Bulls</Card.Title>
           </Card>
-          <Card bg='dark' text='light' border='light'>
+          <Card
+            bg='dark'
+            text='light'
+            border='light'
+            onClick={() => this.generateRandomTweet()}
+          >
             <Card.Img
               variant='top'
               src='https://pbs.twimg.com/profile_images/1281990037/Unknown_400x400.jpeg'
             />
             <Card.Title align='center'>Norm Macdonald</Card.Title>
           </Card>
-          <Card bg='dark' text='light' border='light'>
+          <Card
+            bg='dark'
+            text='light'
+            border='light'
+            onClick={() => this.generateRandomTweet()}
+          >
             <Card.Img
               variant='top'
               src='https://pbs.twimg.com/profile_images/1212786806325821440/reFtNK23.png'
@@ -68,8 +103,17 @@ class RandomPage extends React.Component {
         </CardGroup>
         <hr />
         <br />
-        {/* fake tweets below */}
-        <RandomTweet />
+        {/* fake tweets below - also put info in object in returnedTweet array*/}
+        <RandomTweet
+          userImg={this.state.returnedTweet.userImg}
+          userName={this.state.returnedTweet.userName}
+          userHandle={this.state.returnedTweet.userHandle}
+          date={this.state.returnedTweet.datePosted}
+          tweetBody={this.state.returnedTweet.tweetBody}
+          comments={this.state.returnedTweet.comments}
+          retweets={this.state.returnedTweet.retweets}
+          likes={this.state.returnedTweet.likes}
+        />
       </div>
     );
   }
--- src/Components/RandomTweet.js
+++ src/Components/RandomTweet.js
@@ -2,45 +2,39 @@ import React from "react";
 import { Image, Card, CardGroup, Container, Col, Row } from "react-bootstrap";
 import { FaComment, FaHeart, FaRetweet } from "react-icons/fa";
 
-function RandomTweet() {
+function RandomTweet(props) {
   return (
     <div>
       <Container className='full-tweet'>
         <br />
-        <Image
-          className='profile-pic'
-          src='https://pbs.twimg.com/profile_images/1212786806325821440/reFtNK23.png'
-        />
+        <Image className='profile-pic' src={props.userImg} />
         <Row>
           <Col xs={10} className='tweet-header'>
-            <span className='user-display-name'>Profile Name </span>
-            <span className='user-profile-handle'>@Sample</span>
-            <span className='time-posted'> | 7h</span>
+            <span className='user-display-name'>{props.userName} </span>
+            <span className='user-profile-handle'>@{props.userHandle}</span>
+            <span className='time-posted'> | {props.date}</span>
           </Col>
         </Row>
         <Row>
           <Col xs={11} className='tweet-body'>
-            <span>
-              Tweet body goes here! keep on bloody typing just to see what we
-              get oh my god ok can't stop this just a sample oh babybaby
-            </span>
+            <span>{props.tweetBody}</span>
           </Col>
         </Row>
         <br />
         <Row className='icons'>
           <Col xs={2}>
             <span>
-              <FaComment /> 5
+              <FaComment /> {props.comments}
             </span>
           </Col>
           <Col xs={2}>
             <span>
-              <FaRetweet /> 9
+              <FaRetweet /> {props.retweets}
             </span>
           </Col>
           <Col xs={2}>
             <span>
-              <FaHeart /> 55
+              <FaHeart /> {props.likes}
             </span>
           </Col>
         </Row>
