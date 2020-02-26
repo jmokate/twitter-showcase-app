77f115fcf243cbb6c43490070c5e399ae3aad50c
jmokate <jmokate@gmail.com>
2020-02-26 15:48:17 -0600

    added nodemon

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-console.log(path.parse(__filename));
+app.use(express.static("Client"));
+
+app.get("/", (req, res) => res.send("hello world"));
+
+app.listen(port, () => console.log(`example app is listening on port ${port}`));
