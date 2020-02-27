929c02ef41f378ae689e7c9830182c190b165c87
jmokate <jmokate@gmail.com>
2020-02-26 20:00:06 -0600

    nodemon working changed backend port

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.use(express.static("Client"));
+// serve the static files from react app
+app.use(express.static(path.join(__dirname, "Client/index.html")));
+
+app.use("/Client", express.static("Client"));
 
 app.get("/", (req, res) => res.send("hello world"));
 
-app.listen(port, () => console.log(`example app is lisn port ${port}`));
+app.listen(port, () => console.log(`example app is listening port ${port}`));
