ada1c50dbef0bd3960e5a3101c02f975575438d0
jmokate <jmokate@gmail.com>
2020-03-01 11:47:18 -0600

    added axios on backend

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 
 // serve the static files from react app
 app.use(express.static(path.join(__dirname, "Client/index.html")));
 
 app.use("/Client", express.static("Client"));
 
-app.get("/", (req, res) => res.send("hello world"));
+app.get("/api/client", (req, res) => res.send("hello world"));
 
 app.listen(port, () => console.log(`example app is listening port ${port}`));
