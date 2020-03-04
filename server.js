8264f77aadae7c365a3bddeabb5efe805a7ca119
jmokate <jmokate@gmail.com>
2020-03-03 19:41:20 -0600

    begin work on twitter authentication

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.port || 5000;
+const axios = require("axios");
 
 // serve the static files from react app
-app.use(express.static(path.join(__dirname, "Client/index.html")));
+app.use(express.static(path.join(__dirname, "client/index.html")));
 
-app.use("/Client", express.static("Client"));
+app.use("/client", express.static("client"));
 
-app.get("/api/client", (req, res) => res.send("hello world"));
+app.get("/api/client", (req, res) => {
+  //res.send("hello world")
+
+  axios
+    .get("https://api.twitter.com/1.1/search/tweets.json")
+    .then(response => {
+      res.send(response.data);
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
 
 app.listen(port, () => console.log(`example app is listening port ${port}`));
