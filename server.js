c5865a25575344c343a54a0c7fd7d0915b5cc19b
jmokate <jmokate@gmail.com>
2020-03-05 18:45:35 -0600

    working on authentication

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const path = require("path");
 const app = express();
 const port = process.env.port || 5000;
 const axios = require("axios");
+const bodyParser = require("body-parser");
+
+//config body-parser as middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // serve the static files from react app
 app.use(express.static(path.join(__dirname, "client/index.html")));
 
 app.use("/client", express.static("client"));
+
+app.post("/api/client", (req, res) => {
+  res.send();
+});
 
 app.get("/api/client", (req, res) => {
   //res.send("hello world")
