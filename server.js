8f9045e28d18dc68b4a4c1d6f94b4a7a39772a8d
jmokate <jmokate@gmail.com>
2020-03-10 19:07:53 -0500

    successfully pulling tweets!

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const port = process.env.port || 5000;
 const axios = require("axios");
+require("dotenv").config();
 
 // serve the static files from react app
 app.use(express.static(path.join(__dirname, "client/index.html")));
@@ -12,36 +13,27 @@ app.use("/client", express.static("client"));
 app.get("/api/client", (req, res) => {
   //get bearer token from twitter w. post
 
+  const token = process.env.SECRET_KEY;
+
   const config = {
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    auth: {
-      username: "QfBqPxQTfoJxzvu2VNHZX7SBY",
-      password: "qsTipLXWEZJputrqUalA7BXGyCRpYdKVICrYs79ivM3VJo7SJI"
+    headers: {
+      Authorization: `Bearer ${token}`
     }
   };
 
-  const grantType = "grant_type=client_credentials";
-
   axios
-    .post("https://api.twitter.com/oauth2/token", grantType, config)
+    .get(
+      "https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular",
+      config
+    )
     .then(response => {
-      console.log(response);
-      const token = response.data;
+      res.send(response.data);
+      console.log(response.data);
     })
     .catch(error => {
       console.log(error);
+      res.sendStatus(500);
     });
-
-  // axios
-  //   .get("https://api.twitter.com/1.1/search/tweets.json")
-  //   .then(response => {
-  //     res.send(response.data);
-  //     console.log(response.data);
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //     res.sendStatus(500);
-  //   });
 });
 
 app.listen(port, () => console.log(`example app is listening port ${port}`));
