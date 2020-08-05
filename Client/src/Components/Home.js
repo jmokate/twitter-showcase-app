2e82c1d42a2676f6aa5173a19aa61f0c9ed7d6c1
jmokate <jmokate@gmail.com>
2020-08-05 17:47:45 -0500

    changed home text

--- Client/src/Components/Home.js
+++ Client/src/Components/Home.js
@@ -2,26 +2,28 @@ import React from "react";
 import { Col } from "react-bootstrap";
 
 function Home() {
-  return (
-    <div>
-      <br />
-      <Col align='center'>
-        <h1 className='text'>
-          Twitter API Application
-          <hr />
-        </h1>
-      </Col>
+	return (
+		<div>
+			<br />
+			<Col align='center'>
+				<h1 className='text'>
+					Tweetotaler
+					<hr />
+				</h1>
+			</Col>
 
-      <div id='transparent'></div>
+			<div id='transparent'></div>
 
-      <Col align='center'>
-        <h4 className='text'>
-          Use this app to search Twitter or find random tweets from some of my
-          favorite users.
-        </h4>
-      </Col>
-    </div>
-  );
+			<Col align='center'>
+				<h4 className='text'>
+					Please abstain from other social media platforms other than Twitter,
+					this is Tweetotaler. Use this app to interact with Twitter's API by
+					searching for tweets or viewing random posts from some of my favorite
+					users.
+				</h4>
+			</Col>
+		</div>
+	);
 }
 
 export default Home;
